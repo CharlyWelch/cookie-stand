2f4ee46475c1a712202ea9f15725385c1d050b8e
CharlyWelch <blwbiology@gmail.com>
2017-10-30 13:57:36 -0700

    add files for project, create store objects

---
+++ dom.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const pdx = {
+    min: 23,
+    max: 65,
+    avg: 6.3
+    salesByHour: [],
+    render:
+}
+
+const pioneer = {
+    min: 3,
+    max: 24,
+    avg: 1.2,
+    salesByHour: [],
+    render:
+}
+
+const powells = {
+    min: 11,
+    max: 38,
+    avg: 3.7,
+    salesByHour: [],
+    render:
+}
+
+const stJohns = {
+    min: 20,
+    max: 38,
+    avg: 2.3,
+    salesByHour: [],
+    render:
+}
+
+const waterfront = {
+    min: 2,
+    max: 16,
+    avg: 4.6,
+    salesByHour: [],
+    render:
+}
+
