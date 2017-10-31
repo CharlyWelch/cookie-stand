1d43a7ba79f87c028587f6be57ab6f3f753a4a0c
CharlyWelch <blwbiology@gmail.com>
2017-10-31 15:49:02 -0700

    fix render function, issue with displaying number

--- js/objects.js
+++ js/objects.js
@@ -12,7 +12,7 @@ const pioneer = {
     avg: 1.2,
     hourlySales: hourlySales,
     projHourlyCookies: [],
-    // render: render,
+    render: render,
 }
 
 const powells = {
@@ -22,7 +22,7 @@ const powells = {
     avg: 3.7,
     hourlySales: hourlySales,
     projHourlyCookies: [],
-    // render: render,
+    render: render,
 }
 
 const stJohns = {
@@ -32,7 +32,7 @@ const stJohns = {
     avg: 2.3,
     hourlySales: hourlySales,
     projHourlyCookies: [],
-    // render: render,
+    render: render,
 }
 
 const waterfront = {
@@ -42,7 +42,7 @@ const waterfront = {
     avg: 4.6,
     hourlySales: hourlySales,
     projHourlyCookies: [],
-    // render: render,
+    render: render,
 }
 
 const pdx = {
@@ -52,7 +52,7 @@ const pdx = {
     avg: 6.3,
     hourlySales: hourlySales,
     projHourlyCookies: [],
-    // render: render
+    render: render
 }
 
 function hourlySales(){ 
@@ -60,21 +60,13 @@ function hourlySales(){
     for (let i=0; i < 14; i++){
         const numCustomers = getRandomIntInclusive(this.min, this.max);
         const numCookies = Math.round(numCustomers * this.avg);
-        const oneHour = {
-            cookiesSold: numCookies
-        }
-        projHourlyCookies.push(oneHour);
+        projHourlyCookies.push(numCookies);
     }
+    this.projHourlyCookies = projHourlyCookies;  
 }
 
 pdx.hourlySales();
-console.log(pdx);
-
-waterfront.hourlySales();
-console.log(waterfront);
-
-
-//// from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+console.log(pdx)
 
 function getRandomIntInclusive(min, max){
     min = Math.ceil(min);
@@ -82,27 +74,26 @@ function getRandomIntInclusive(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
 function render(){
-    for( let i = 0; i < stores.length; i++){
-        const hourlySalesList = document.getElementById('hourly-sales');
-        const title = document.createElement(H2);
-        title.textContent = this.name;
-        const ul = document.createElement(UL);
-        hourlySalesList.appendChild(title);
-        hourlySalesList.appendChild(ul);
+    const hourlySalesList = document.getElementById('hourly-sales');
+    const title = document.createElement('H2');
+    title.textContent = this.name;
+    const ul = document.createElement('UL');
+    hourlySalesList.appendChild(title);
+    hourlySalesList.appendChild(ul);
 
-            for (let i = 0; i < hours.length; i++){
-                let li = document.createElement(LI);
-                li.textContent = this.hours[i] + ' ' + this.projHourlyCookies[i];
-                ul.appendChild(li);
-            }
+        for (let i = 0; i < hours.length; i++){
+            let li = document.createElement('LI');
+            li.textContent = hours[i] + '  ' + this.projHourlyCookies[i];
+            ul.appendChild(li);
         }
 }
+pioneer.render();
+powells.render();
+stJohns.render()
+waterfront.render();
+pdx.render();
 
-render();
 
 
-    
-
+//// Random number generator from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
